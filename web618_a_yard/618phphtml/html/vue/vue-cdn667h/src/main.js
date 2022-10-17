c43e8b954d5c08bf5bfa3577242716b905f84a52
David Gleba <dgleba@gmail.com>
2022-10-16 22:06:21 -0400

    2022-10-16c

---
+++ web618_a_yard/618phphtml/html/vue/vue-cdn667h/src/main.js
@@ -0,0 +1,55 @@
+// =================================================
+/*
+
+This is a small vue app to edit a table in one component.
+There is no auth/login on the table.
+
+*/
+// =================================================
+
+const options = {
+  moduleCache: { vue: Vue },
+
+  async getFile(url) {
+    const res = await fetch(url);
+    if ( !res.ok ) {
+      throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+    }
+    return {
+      getContentData: (asBinary) => asBinary ? res.arrayBuffer() : res.text(),
+    }
+  },
+  addStyle: () => {},
+}
+
+// =================================================
+
+var Home = Vue.extend({
+  // mixins: [util],
+  template: '#home'
+});
+
+// =================================================
+
+function load(path) {
+  return window['vue2-sfc-loader'].loadModule(path, options);
+}
+
+Vue.use(VueRouter);
+
+// =================================================
+
+load('./src/app.vue')
+.then(app => {
+  const router = new VueRouter({
+    //mode: 'history',
+    mode: 'hash',
+    routes: [
+      { path: '/h', component: () => load('./src/hello.vue') },
+      { path: '/pm', component: () => load('./src/postsManager.vue') },
+      { path: '/login', component: () => load('./src/login.vue') },
+      { path: '/', component: Home},
+    ]
+  })
+  new Vue({ ...app, router }).$mount('#app')
+});
