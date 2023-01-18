b1864fde33d2c9c5fc194c3807b31f11b8276c7c
David Gleba <dgleba@gmail.com>
2023-01-17 19:31:02 -0500

    2023-01-17-strap179b

---
+++ web618_a_yard/test/strap179b/docs/index-2016a.php
@@ -0,0 +1,9 @@
+<?php require 'templates/head.php';?>
+<?php require 'nav.php';?>
+
+
+<?php require 'indexbody.php';?>
+<?php //require 'templates/spacer.php';?>
+
+
+<?php require 'templates/foot.php';?>
---
+++ web618_a_yard/test/strap179b/page2.php
@@ -0,0 +1,8 @@
+<?php require 'templates/head.php';?>
+<?php require 'nav.php';?>
+
+
+<?php require 'page2body.php';?>
+
+
+<?php require 'templates/foot.php';?>
