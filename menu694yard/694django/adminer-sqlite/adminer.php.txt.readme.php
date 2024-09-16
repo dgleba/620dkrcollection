17c84f6c28fa6c87a910e31153f8df31a1e0361e
David Gleba <dgleba@gmail.com>
2024-09-16 12:07:25 -0400

    2024-09-16a

---
+++ menu694yard/694django/adminer-sqlite/adminer.php.txt.readme.php
@@ -0,0 +1,9 @@
+If adminer.php is missing.
+
+Download adminer.php and put it here.
+
+Why:
+sophos has a false positive on adminer.php. I may exclude it to avoid false malware warning on my systems.
+
+2024-09-15_Sun_21.42-PM
+David Gleba
